ef0a7f1dbfed4f55cffee53f29a9b2208e90f416
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-08-31 22:01:16 +0900

    Shots passed to backend now go to database.

--- controllers/shotController.js
+++ controllers/shotController.js
@@ -3,8 +3,8 @@ const shotDb = require("../db/shotQueries");
 require('dotenv').config();
 
 async function addShotToSession(req, res) {
-    const {sessionId, x, y} = req.body;
-    const newShot = await shotDb.addShotToSession(sessionId, x, y);
+    const {sessionId, userId, x, y, hit, kinteki} = req.body;
+    const newShot = await shotDb.addShotToSession(sessionId, userId, x, y, hit, kinteki);
     return res.status(201).json({
         message: 'Shot created successfully',
         shot: newShot
--- db/shotQueries.js
+++ db/shotQueries.js
@@ -1,13 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-async function addShotToSession(sessionId, x, y){
+async function addShotToSession(sessionId, userId, x, y, hit, kinteki){
     const shot = await prisma.shot.create({
         data: {
+            userId: userId,
             sessionId: sessionId,
             timestamp: new Date(),
             x: x,
-            y: y
+            y: y,
+            hit: hit,
+            kinteki: kinteki
         },
     });
     return shot;
